82f4d5f66ac41083369e2220d29e8a05c32f744a
Salvatore Trimarchi <path72@gmail.com>
2021-07-21 11:31:57 +0200

    cleaning for portfolio

--- js/main.js
+++ js/main.js
@@ -31,66 +31,7 @@ var app = new Vue(
 							date: '10/01/2020 16:15:22',
 							text: 'Tutto fatto!',
 							status: 'received'
-						},
-						/////////////
-						{
-							date: '10/01/2020 15:30:55',
-							text: 'Hai portato a spasso il cane?',
-							status: 'sent'
-						},
-						{
-							date: '10/01/2020 15:50:00',
-							text: 'Ricordati di dargli da mangiare',
-							status: 'sent'
-						},
-						{
-							date: '10/01/2020 16:15:22',
-							text: 'Tutto fatto!',
-							status: 'received'
-						},						{
-							date: '10/01/2020 15:30:55',
-							text: 'Hai portato a spasso il cane?',
-							status: 'sent'
-						},
-						{
-							date: '10/01/2020 15:50:00',
-							text: 'Ricordati di dargli da mangiare',
-							status: 'sent'
-						},
-						{
-							date: '10/01/2020 16:15:22',
-							text: 'Tutto fatto!',
-							status: 'received'
-						},						{
-							date: '10/01/2020 15:30:55',
-							text: 'Hai portato a spasso il cane?',
-							status: 'sent'
-						},
-						{
-							date: '10/01/2020 15:50:00',
-							text: 'Ricordati di dargli da mangiare',
-							status: 'sent'
-						},
-						{
-							date: '10/01/2020 16:15:22',
-							text: 'Tutto fatto!',
-							status: 'received'
-						},						{
-							date: '10/01/2020 15:30:55',
-							text: 'Hai portato a spasso il cane?',
-							status: 'sent'
-						},
-						{
-							date: '10/01/2020 15:50:00',
-							text: 'Ricordati di dargli da mangiare',
-							status: 'sent'
-						},
-						{
-							date: '10/01/2020 16:15:22',
-							text: 'Tutto fatto!',
-							status: 'received'
 						}
-						////////////
 					],
 				},
 				{	name: 'Fabio',
